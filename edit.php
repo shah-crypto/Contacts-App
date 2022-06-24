83c99b40a3f1d0f6732b388a9f4e781c75a347bc
Pratham Shah <prathamshah2507@gmail.com>
2022-06-24 22:27:21 +0530

    Update the edit page

--- edit.php
+++ edit.php
@@ -2,6 +2,20 @@
 include 'base.php';
 ?>
 
+<?php
+
+if (isset($_POST['save'])){
+    $ed_id = $_POST['ed_id'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $number = $_POST['number'];
+    $query1 = "UPDATE `contacts` SET `fname` = '$first_name', `lname` = '$last_name', `phone` = '$number' WHERE `contacts`.`id` = $ed_id";
+    $res1 = mysqli_query($conn, $query1);
+    header("Location: index.php");
+}
+
+?>
+
 <title>Edit contact</title>
 
 <style>
@@ -12,8 +26,7 @@ include 'base.php';
 </style>
 
 <?php
-if (isset($_GET['edit']))
-{
+if (isset($_GET['edit'])) {
     $eid = $_GET['edit'];
     $query = "SELECT * FROM contacts WHERE id = $eid";
     $res = mysqli_query($conn, $query);
@@ -31,7 +44,8 @@ if (isset($_GET['edit']))
 
 <body>
     <div class="container my-5">
-        <form action="add.php" method="post">
+        <form action="edit.php" method="post">
+            <input type="hidden" name="ed_id" value="<?php echo $uid; ?>">
             <div class="mb-3">
                 <label for="first_name" class="form-label">First name</label>
                 <input type="text" id="first_name" name="first_name" class="form-control" value="<?php echo $ufname; ?>" required>
