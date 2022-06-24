c9127d9e7de52c0c53f1e83aee905bb534e84d32
Pratham Shah <prathamshah2507@gmail.com>
2022-06-24 19:16:51 +0530

    New contacts page

--- add.php
+++ add.php
@@ -2,8 +2,37 @@
 include 'base.php';
 ?>
 
+<?php
+
+if(isset($_POST['add']))
+{
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $number = $_POST['number'];
+    $query = "INSERT INTO `contacts` (`id`, `fname`, `lname`, `phone`) VALUES (NULL, '$first_name', '$last_name', '$number')";
+    $res = mysqli_query($conn, $query);
+}
+
+?>
+
+<title>Add contact</title>
+
 <body>
     <div class="container my-5">
-        <h1>Hey</h1>
+        <form action="add.php" method="post">
+            <div class="mb-3">
+                <label for="first_name" class="form-label">Enter the first name</label>
+                <input type="text" id="first_name" name="first_name" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="last_name" class="form-label">Enter the last name</label>
+                <input type="text" id="last_name" name="last_name" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="number" class="form-label">Enter the 10-digit number</label>
+                <input type="text" id="number" name="number" class="form-control" required>
+            </div>
+            <input type="submit" value="Add to contacts" name="add" class="btn btn-primary">
+        </form>
     </div>
 </body>
